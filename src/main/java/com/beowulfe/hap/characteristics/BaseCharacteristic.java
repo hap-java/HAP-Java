1b35063dfee59009ba2cecdfc7d752a61bcd283b
Christophe Dirac <cdirac@me.com>
2016-06-16 20:32:55 +0200

    Don't write a null value to serialized characteristics, HomeKit cannot handle that.

--- src/main/java/com/beowulfe/hap/characteristics/BaseCharacteristic.java
+++ src/main/java/com/beowulfe/hap/characteristics/BaseCharacteristic.java
@@ -172,7 +172,7 @@ public abstract class BaseCharacteristic<T> implements Characteristic {
 		} else if (value instanceof BigDecimal){
 			builder.add("value", (BigDecimal) value);
 		} else if (value == null) {
-			builder.addNull("value");
+			// Do not add null value, HomeKit cannot handle that
 		} else {
 			builder.add("value", value.toString());
 		}
