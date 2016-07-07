e49fcc0f24dc236b2a126f1a3854a5fb0fb1e8b1
Gaston Dombiak <gdombiak@gmail.com>
2016-07-06 23:36:46 -0700

    Added support for motion sensors and contact sensors

---
+++ src/main/java/com/beowulfe/hap/impl/services/ContactSensorService.java
@@ -0,0 +1,14 @@
+package com.beowulfe.hap.impl.services;
+
+import com.beowulfe.hap.accessories.ContactSensor;
+import com.beowulfe.hap.impl.characteristics.common.Name;
+import com.beowulfe.hap.impl.characteristics.contactsensor.ContactSensorStateCharacteristic;
+
+public class ContactSensorService extends AbstractServiceImpl {
+
+    public ContactSensorService(ContactSensor contactSensor) {
+        super("00000080-0000-1000-8000-0026BB765291");
+        addCharacteristic(new Name(contactSensor));
+        addCharacteristic(new ContactSensorStateCharacteristic(contactSensor));
+    }
+}
