c3908f26f905ae3f0098e75c3d058a202061d41e
Gaston Dombiak <gdombiak@gmail.com>
2016-07-21 19:09:34 -0700

    Added support for carbon monoxide sensor and smoke sensor.

---
+++ src/main/java/com/beowulfe/hap/accessories/SmokeSensor.java
@@ -0,0 +1,46 @@
+package com.beowulfe.hap.accessories;
+
+import com.beowulfe.hap.HomekitAccessory;
+import com.beowulfe.hap.HomekitCharacteristicChangeCallback;
+import com.beowulfe.hap.Service;
+import com.beowulfe.hap.accessories.properties.SmokeDetectedState;
+import com.beowulfe.hap.impl.services.SmokeSensorService;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * <p>A smoke sensor reports whether smoke has been detected or not.</p>
+ *
+ * <p>Smoke sensors that run on batteries will need to implement this interface
+ * and also implement {@link BatteryAccessory}.</p>
+ *
+ * @author Gaston Dombiak
+ */
+public interface SmokeSensor extends HomekitAccessory {
+
+    /**
+     * Retrieves the state of the smoke sensor. This is whether smoke has been detected or not.
+     *
+     * @return a future that will contain the smoke sensor's state
+     */
+    CompletableFuture<SmokeDetectedState> getSmokeDetectedState();
+
+    @Override
+    default Collection<Service> getServices() {
+        return Collections.singleton(new SmokeSensorService(this));
+    }
+
+    /**
+     * Subscribes to changes in the smoke sensor's state.
+     *
+     * @param callback the function to call when the state changes.
+     */
+    void subscribeSmokeDetectedState(HomekitCharacteristicChangeCallback callback);
+
+    /**
+     * Unsubscribes from changes in the smoke sensor's state.
+     */
+    void unsubscribeSmokeDetectedState();
+}
---
+++ src/main/java/com/beowulfe/hap/impl/characteristics/carbonmonoxide/Carbon/CarbonMonoxideDetectedCharacteristic.java
@@ -0,0 +1,38 @@
+package com.beowulfe.hap.impl.characteristics.carbonmonoxide.Carbon;
+
+import com.beowulfe.hap.HomekitCharacteristicChangeCallback;
+import com.beowulfe.hap.accessories.CarbonMonoxideSensor;
+import com.beowulfe.hap.accessories.properties.CarbonMonoxideDetectedState;
+import com.beowulfe.hap.characteristics.EnumCharacteristic;
+import com.beowulfe.hap.characteristics.EventableCharacteristic;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CarbonMonoxideDetectedCharacteristic extends EnumCharacteristic implements EventableCharacteristic {
+
+    private final CarbonMonoxideSensor carbonMonoxideSensor;
+
+    public CarbonMonoxideDetectedCharacteristic(CarbonMonoxideSensor carbonMonoxideSensor) {
+        super("00000069-0000-1000-8000-0026BB765291", false, true, "Carbon Monoxide Detected", 1);
+        this.carbonMonoxideSensor = carbonMonoxideSensor;
+    }
+
+    @Override
+    protected CompletableFuture<Integer> getValue() {
+        return carbonMonoxideSensor.getCarbonMonoxideDetectedState().thenApply(CarbonMonoxideDetectedState::getCode);
+    }
+
+    @Override
+    protected void setValue(Integer value) throws Exception {
+        //Read Only
+    }
+
+    @Override
+    public void subscribe(HomekitCharacteristicChangeCallback callback) {
+        carbonMonoxideSensor.subscribeCarbonMonoxideDetectedState(callback);
+    }
+
+    @Override
+    public void unsubscribe() {
+        carbonMonoxideSensor.unsubscribeCarbonMonoxideDetectedState();
+    }}
