f3a5f2b712a2b5800535de5d6e09907284be6f77
Tim Harper <timcharper@gmail.com>
2019-01-03 21:10:52 -0700

    Add Valve type
    Adds support for regular on/off valves, and ValveWithTimer

---
+++ src/main/java/com/beowulfe/hap/impl/characteristics/common/ActiveCharacteristic.java
@@ -0,0 +1,47 @@
+package com.beowulfe.hap.impl.characteristics.common;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import com.beowulfe.hap.HomekitCharacteristicChangeCallback;
+import com.beowulfe.hap.characteristics.BooleanCharacteristic;
+import com.beowulfe.hap.characteristics.EventableCharacteristic;
+import com.beowulfe.hap.impl.ExceptionalConsumer;
+
+public class ActiveCharacteristic extends BooleanCharacteristic implements EventableCharacteristic {
+
+    private final Supplier<CompletableFuture<Boolean>> getter;
+    private final ExceptionalConsumer<Boolean> setter;
+    private final Consumer<HomekitCharacteristicChangeCallback> subscriber;
+    private final Runnable unsubscriber;
+
+    public ActiveCharacteristic(Supplier<CompletableFuture<Boolean>> getter, ExceptionalConsumer<Boolean> setter,
+            Consumer<HomekitCharacteristicChangeCallback> subscriber, Runnable unsubscriber) {
+        super("000000B0-0000-1000-8000-0026BB765291", true, true, "Active");
+        this.getter = getter;
+        this.setter = setter;
+        this.subscriber = subscriber;
+        this.unsubscriber = unsubscriber;
+    }
+
+    @Override
+    protected CompletableFuture<Boolean> getValue() {
+        return getter.get();
+    }
+
+    @Override
+    protected void setValue(Boolean value) throws Exception {
+        setter.accept(value);
+    }
+
+    @Override
+    public void subscribe(HomekitCharacteristicChangeCallback callback) {
+        subscriber.accept(callback);
+    }
+
+    @Override
+    public void unsubscribe() {
+        unsubscriber.run();
+    }
+}
