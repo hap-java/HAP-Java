9c91d828145740bdde2429c4d7874a5b01909a20
Eugen Freiter <freiter@gmail.com>
2020-03-17 23:52:57 +0100

    Apply J-N-K patches for Openhab. update dependencies to be aligned with OH

--- src/main/java/io/github/hapjava/server/impl/http/impl/LoggingHandler.java
+++ src/main/java/io/github/hapjava/server/impl/http/impl/LoggingHandler.java
@@ -2,16 +2,14 @@ package io.github.hapjava.server.impl.http.impl;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import org.apache.commons.io.HexDump;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoggingHandler extends ChannelDuplexHandler {
 
-  private static final Logger logger = LoggerFactory.getLogger(NettyHomekitHttpService.class);
+  private static final Logger logger = LoggerFactory.getLogger(LoggingHandler.class);
+  private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
   @Override
   public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -32,19 +30,27 @@ public class LoggingHandler extends ChannelDuplexHandler {
 
   private void logBytes(String type, ByteBuf buf, ChannelHandlerContext ctx) throws IOException {
     if (buf.readableBytes() > 0) {
-      try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
-        byte[] bytes = new byte[buf.readableBytes()];
-        buf.getBytes(0, bytes, 0, bytes.length);
-        HexDump.dump(bytes, 0, stream, 0);
-        stream.flush();
-        logger.trace(
-            String.format(
-                "%s %s [%s]:%n%s%n",
-                type,
-                buf,
-                ctx.channel().remoteAddress().toString(),
-                stream.toString(StandardCharsets.UTF_8.name())));
-      }
+      byte[] bytes = new byte[buf.readableBytes()];
+      buf.getBytes(0, bytes, 0, bytes.length);
+      logger.trace(
+          String.format(
+              "%s %s [%s]:%n%s%n",
+              type, buf, ctx.channel().remoteAddress().toString(), bytesToHex(bytes)));
     }
+  }
+
+  /*
+  This method is licensed under CC-BY-SA and published on Stackoverflow
+  https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
+  from the answer by Melquiades https://stackoverflow.com/users/2964945/melquiades
+  */
+  private String bytesToHex(byte[] bytes) {
+    char[] hexChars = new char[bytes.length * 2];
+    for (int j = 0; j < bytes.length; j++) {
+      int v = bytes[j] & 0xFF;
+      hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+      hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+    }
+    return new String(hexChars);
   }
 }
