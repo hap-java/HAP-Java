98efd976abfee5d2e63e45b3bbce52019788d03b
Andy Lintner <dev@beowulfe.com>
2021-03-28 16:44:59 -0400

    Remove old deploy mechanism

--- deploy/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ ( $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ) ]]; then
-    mvn deploy --settings deploy/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
-fi
